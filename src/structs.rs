dbdbbb8508c56b87cc76c104c3ba4944c73b889b
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-01-12 23:50:08 +0530

    added more structs

--- src/structs.rs
+++ src/structs.rs
@@ -2,21 +2,35 @@ use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct Post {
-    pub id: i32,
+    pub id: u32,
     pub name: String,
     pub body: String,
-    pub likes: i32,
-    pub dislikes: i32,
-    pub author_id: i32
+    pub likes: u32,
+    pub dislikes: u32,
+    pub author_id: u32
 }
 
 #[derive(Serialize)]
 pub struct PostSmall{
     pub name: String,
-    pub id: i32
+    pub id: u32
 }
 
 #[derive(Serialize)]
 pub struct Posts{
     pub posts: Vec<PostSmall>
+}
+
+#[derive(Serialize)]
+pub struct Comment{
+    pub id: u32,
+    pub body: String,
+    pub author_id: u32
+}
+
+
+#[derive(Serialize)]
+pub struct Author{
+    pub name: String,
+    pub id: u32
 }
